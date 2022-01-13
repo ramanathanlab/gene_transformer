a3bba0d984c73ad4706c9972648b5f8d06e7d7e6
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2022-01-12 16:04:34 -0800

    argparse import

--- gene_transformer/model.py
+++ gene_transformer/model.py
@@ -36,6 +36,7 @@ import torch.distributed as dist
 import torch.multiprocessing as mp
 from torch.nn.parallel import DistributedDataParallel
 from torch.utils.data.distributed import DistributedSampler
+from argparse import ArgumentParser
 
 
 # from https://github.com/spellml/deeplab-voc-2012/blob/master/models/2_pytorch_distributed_model.py
